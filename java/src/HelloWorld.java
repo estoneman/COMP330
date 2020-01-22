c437160f951fbb1ee03e5407b1e5dda25a71827a
Ethan Stoneman <estoneman@luc.edu>
2020-01-21 22:22:50 -0600

    got json-simple to work

--- java/src/HelloWorld.java
+++ java/src/HelloWorld.java
@@ -1,7 +1,13 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+//in order for these import statements to work I used the following linux command:
+//export CLASSPATH=/path/to/json-simple.jar/file
 
 class HelloWorld {
 
     public static void main(String[] args) {
+        JSONObject jO = new JSONObject();
 
     }
 }
