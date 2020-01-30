b009badd95b7564e3a327f87e1306882254b1d64
Ethan Stoneman <estoneman@luc.edu>
2020-01-30 16:44:50 -0600

    Erased all unnecessary code

--- java/src/HelloWorld.java
+++ java/src/HelloWorld.java
@@ -3,6 +3,8 @@ import org.json.simple.JSONObject;
 import java.io.FileReader;
 import org.json.simple.parser.*;
 
+//Inspriation from: https://www.geeksforgeeks.org/parse-json-java/
+
 //in order for these import statements to work I used the following linux command:
 //export CLASSPATH=/path/to/json-simple.jar/file
 //having an issue where I cannot run the HelloWorld object it compiles fine and everything it just will not run
@@ -11,23 +13,8 @@ import org.json.simple.parser.*;
 class HelloWorld {
 
     public static void main(String[] args) {
-        JSONObject jO = new JSONObject();
 
         try {
-            // parsing file "JSONExample.json"
-            //change the file location to whatever it is on your computer
-            Object obj = new JSONParser().parse(new FileReader("Users/Ethan/AndroidStudioProjects/COMP330/json/sample.json"));
-
-            // typecasting obj to JSONObject
-            JSONObject jo = (JSONObject) obj;
-
-            //just testing to see if it will get our last names from sample.json
-            String lastName1 = (String) jo.get("lastName1");
-            String lastName2 = (String) jo.get("lastName2");
-
-            System.out.println(lastName1);
-            System.out.println(lastName2);
-        }
 
         catch(Exception e) {
             System.out.println(e);
