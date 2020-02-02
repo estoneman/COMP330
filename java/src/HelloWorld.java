9fafe7d03d6723c10650512eeb1753f21ce18c9b
thomasdimonte <tdimonte@luc.edu>
2020-02-02 17:42:17 -0600

    added other class path for file reader

--- java/src/HelloWorld.java
+++ java/src/HelloWorld.java
@@ -19,6 +19,7 @@ class HelloWorld {
         try {
             //read json file into this class in order to parse
             Object fileRead = new JSONParser().parse(new FileReader("/Users/Ethan/AndroidStudioProjects/COMP330/json/sample.json"));
+            //Object fileRead = new JSONParser().parse(new FileReader("/Users/thomasdimonte/StudioProjects/COMP330/json/sample.json"));
 
             // typecasting obj to JSONObject in order to use JSONSimple library
             JSONObject jsonObject = (JSONObject) fileRead;
