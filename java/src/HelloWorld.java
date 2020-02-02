addf56db54f61a5eb27558ada922c1460ff4b600
Ethan Stoneman <estoneman@luc.edu>
2020-02-02 15:52:37 -0600

    Figured out how to populate hash map with true/false questions by reading in sample.json

--- java/src/HelloWorld.java
+++ java/src/HelloWorld.java
@@ -8,36 +8,59 @@ import java.util.*;
 //Inspriation from: https://www.geeksforgeeks.org/parse-json-java/
 
 //in order for these import statements to work I used the following linux command:
-//export CLASSPATH=/path/to/json-simple.jar/file
-//having an issue where I cannot run the HelloWorld object it compiles fine and everything it just will not run
-//returns a java.lang.ClassNotFoundException every time
+//export CLASSPATH=$CLASSPATH:/path/to/json-simple.jar/file
 
 class HelloWorld {
 
-    public static void main(String[] args) {
+    private static HashMap<String, String> trueFalseHashMap = new HashMap<String, String>();
+
+    private HashMap<String, String> populateHashmap() {
 
         try {
-            Object obj = new JSONParser().parse(new FileReader("/Users/Ethan/AndroidStudioProjects/COMP330/json/sample.json"));
+            //read json file into this class in order to parse
+            Object fileRead = new JSONParser().parse(new FileReader("/Users/Ethan/AndroidStudioProjects/COMP330/json/sample.json"));
 
-            // typecasting obj to JSONObject
-            JSONObject jo = (JSONObject) obj;
+            // typecasting obj to JSONObject in order to use JSONSimple library
+            JSONObject jsonObject = (JSONObject) fileRead;
 
-            JSONArray ja = (JSONArray) jo.get("tf");
+            //getting our T/F question; in this case we stored the question and answers as array
+            JSONArray trueFalseArray = (JSONArray) jsonObject.get("tf");
 
-            Iterator iter = ja.iterator();
+            //to iterate through objects in array of t/f
+            Iterator<?> iter = trueFalseArray.iterator();
 
-            System.out.println(jo.names());
+            //for accessing the objects inside trueFalseArray and converting to Strings
+            //for the purpose of parsing by indexing of each String element
+            String questionAnswerPair;
 
-//            while (iter.hasNext()) {
-//
-//            }
+            //for separating questions and answers into their own index for easier parsing
+            String[] questionAnswerArray;
+
+            //iterates through the JSONArray and populates true false hash map
+            while (iter.hasNext()) {
+                questionAnswerArray = parseTFObject(iter.next());
+
+                String question = questionAnswerArray[0].substring(5, questionAnswerArray[0].length() - 1);//gets rid of unneeded characters
+                String answer = questionAnswerArray[1].substring(5, questionAnswerArray[1].length() - 1);//gets rid of unneeded characters
+
+                trueFalseHashMap.put(question, answer);//populates the hash map with correct values
+            }
 
         }
 
         catch(Exception e) {
             System.out.println(e);
-
         }
+
+        return trueFalseHashMap;//populated hash map with questions and answers of the true false section in json file
+
+    }
+
+    private String[] parseTFObject(Object obj) {
+        String objString = obj.toString();//converts iter.next() which is of type Object to String
+        objString = objString.substring(1, objString.length() - 1);//gets rid of leading and trailing braces in string
+        String[] result = objString.split(",");//puts elements into their own index for easier parsing
+        return result;
     }
 
 }
