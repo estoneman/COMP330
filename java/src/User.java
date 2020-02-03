c44018c4f0bd75a7b416bf49c23cd23201a15a30
thomasdimonte <tdimonte@luc.edu>
2020-02-03 09:25:59 -0600

    Added User.java, here we will interact with the user based on information we read from the json

---
+++ java/src/User.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+        //this class will be used to interact with the user
+
+        String name; //for scoring
+        String qType; //(tf,mc,fb,m, or all)
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        name = keyboard.nextLine();
+
+        System.out.println("Select question type: tf, mc, fb, m, or all"); //eventually we want to only offer q's avaliable from json
+        qType = keyboard.nextLine();
+
+    }
+}
