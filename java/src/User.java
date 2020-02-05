b1152f43485825406c5b984700f5fda0d78f3522
thomasdimonte <tdimonte@luc.edu>
2020-02-05 09:48:55 -0600

    added switch statements and userChoice to User.java

--- java/src/User.java
+++ java/src/User.java
@@ -1,31 +1,64 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import java.io.FileReader;
-import org.json.simple.parser.*;
+//import org.json.simple.JSONArray;
+//import org.json.simple.JSONObject;
+//import java.io.FileReader;
+//import org.json.simple.parser.*;
 
 import java.util.Scanner;
 
-public class User {
+public class User extends ReadJSONFile {
 
-    ReadJSONFile readJSONFile = new ReadJSONFile();
+//    ReadJSONFile readJSONFile = new ReadJSONFile();
+//    private HashMap<String, String> JSONHashMap;
 
     public static void main(String[] args) {
         //this class will be used to interact with the user
 
-        ReadJSONFile readJSONFile = new ReadJSONFile();
+//        ReadJSONFile readJSONFile = new ReadJSONFile();
 
         String name; //for scoring
         String qType; //(tf,mc,fb,m, or all)
+        String userChoice; //between (A)Create a quiz or (B)Take a quiz
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter your name: ");
         name = keyboard.nextLine();
 
-        System.out.println("Select question type: tf, mc, fb, m, or all"); //eventually we want to only offer q's avaliable from json
-        qType = keyboard.nextLine();
+        System.out.println("Would you like to (A)Create a quiz or (B)Take a quiz?"); //or (C)Exit
+        userChoice = keyboard.nextLine();
 
-        //make switch case statement for each qType
+        if userChoice.equalsIgnoreCase("A") {
+            //Ask reader for q's and a's and format them into JSON file
 
+        }
+
+        if userChoice.equalsIgnoreCase("B") {
+            System.out.println("Select question type: tf, mc, fb, m, or all");
+            //eventually we want to only offer q types avaliable from json
+            //and allow users to select multiple q types to form their own quiz, ex tf + fb
+            qType = keyboard.nextLine();
+
+            //switch case statement for each qType
+            //check if case sensitive!
+            switch (qType) {
+                case "tf":
+                    //get tf from hashmap
+                    break;
+
+                case "mc":
+                    //get mc from hashmap
+                    break;
+
+                case "fb":
+                    //get fb from hashmap
+                    break;
+
+                case "m":
+                    //get m from hashmap
+                    break;
+            }
+        }
+
+//        JSONHashMap = new HashMap<String, String>();
 
         System.out.println(JSONHashmap);
 
